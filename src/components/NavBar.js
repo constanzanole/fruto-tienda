111a314b6102a982e6d7a603edd74e4d32eb6cc1
Constanza Nole <cotynole@gmail.com>
2021-10-12 21:38:04 -0300

    nav bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+import '../App.css';
+
+function NavBar() {
+	const [showLinks, setShowLinks] = useState(false);
+
+	return (
+		<div className="Navbar">
+			<div className="leftSide">
+				<div className="links" id={showLinks ? 'hidden' : ''}>
+					<a href="/collares">Collares</a>
+					<a href="/aros">Aros</a>
+					<a href="/broches">Broches</a>
+					<a href="/anllos">Anillos</a>
+					<a href="/pulseras">Pulseras</a>
+				</div>
+				{window.screen.width <= 650 && (
+					<AiOutlineMenu
+						onClick={() => setShowLinks(!showLinks)}
+						color={'white'}
+						size={30}
+						className="menu-icon"
+					>
+						FRUTO
+					</AiOutlineMenu>
+				)}
+			</div>
+			<div className="rightSide">
+				<input type="text" placeholder="Buscar...." />
+				<button>Buscar</button>
+			</div>
+		</div>
+	);
+}
+
+export default NavBar;
